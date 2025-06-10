b9e4357457b0c8e5d911f341618da0b5c71785a8
w89114 <w89114@naver.com>
2025-06-11 08:07:28 +0900

    공지사항

---
+++ yami/src/main/java/com/gaonna/yami/notice/model/service/NoticeServiceImpl.java
@@ -0,0 +1,71 @@
+package com.gaonna.yami.notice.model.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gaonna.yami.common.PageInfo;
+import com.gaonna.yami.notice.model.dao.NoticeDao;
+import com.gaonna.yami.notice.model.vo.Notice;
+
+@Service
+public class NoticeServiceImpl implements NoticeService {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Autowired
+    private NoticeDao noticeDao;
+
+    @Override
+    public List<Notice> selectNoticeList() {
+        return noticeDao.selectNoticeList(sqlSession);
+    }
+    
+    @Override
+    public Notice selectNotice(int noticeNo) {
+        return noticeDao.selectNotice(sqlSession, noticeNo);
+    }
+
+    @Override
+    public int updateCount(int noticeNo) {
+        return noticeDao.updateCount(sqlSession, noticeNo);
+    }
+
+    @Override
+    public int insertNotice(Notice 	notice) {
+        return noticeDao.insertNotice(sqlSession, notice);
+    }
+
+    @Override
+    public int updateNotice(Notice notice) {
+        return noticeDao.updateNotice(sqlSession, notice);
+    }
+
+    @Override
+    public int deleteNotice(int noticeNo) {
+        return noticeDao.deleteNotice(sqlSession, noticeNo);
+    }
+    
+    @Override
+    public int selectListCount() {
+        return noticeDao.selectListCount(sqlSession);
+    }
+    
+    @Override
+    public List<Notice> selectList(PageInfo pi) {
+        int startRow = (pi.getCurrentPage() - 1) * pi.getBoardLimit() + 1;
+        int endRow = startRow + pi.getBoardLimit() - 1;
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("startRow", startRow);
+        map.put("endRow", endRow);
+        map.put("pi", pi); 
+
+        return noticeDao.selectList(sqlSession, map);
+    }
+}
